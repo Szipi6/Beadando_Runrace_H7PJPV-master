73e338d158fdd3e89763de2b3dec19c3ca516c3c
Szipi6 <szabinahm@freemail.hu>
2024-05-17 12:26:57 +0200

    application.java

---
+++ src/main/java/eu/runnerrace/h7pjpv/H7pjpvApplication.java
@@ -0,0 +1,13 @@
+package eu.runnerrace.h7pjpv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H7pjpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H7pjpvApplication.class, args);
+	}
+
+}
